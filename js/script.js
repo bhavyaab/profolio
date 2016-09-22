264b9a107d9c208f4c644881bb13972e160b797b
Bhavya Bharti <bhavyab@outlook.com>
2016-09-22 11:25:05 -0700

    select box added

--- js/script.js
+++ js/script.js
@@ -1,7 +1,18 @@
-var article = [];
+// var article = [];
 
-var source = $('#home-template').html();
-var template = Handlebars.compile(source);
-var theCompiledHtml = template(myContext);
-$('.home').html(theCompiledHtml);
-console.log(theCompiledHtml);
+// function Article (opts) {  //declere constructor function to construct the 'Article' object
+//   for (var keys in opts) {  //for loop will run for
+//     this[keys] = opts[keys];
+//   }
+// }
+
+var source = $('#home-template').html();  //fetch the handelbar template
+var template = Handlebars.compile(source); //compile it
+var theCompiledHtml = template(myContext); //
+$('.home').append(theCompiledHtml);
+
+//filter creation
+var sourceC = $('#category-option-template').html();
+var templateC = Handlebars.compile(sourceC);
+var theCompiledHtmlC = templateC(myContext);
+$('#category-filter').append(theCompiledHtmlC);

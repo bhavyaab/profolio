535f3409cce1cfbe36c002d147360f1c42a70e8f
Bhavya Bharti <bhavyab@outlook.com>
2016-09-22 12:12:03 -0700

    filter is working

--- js/viewPage.js
+++ js/viewPage.js
@@ -1,7 +1,15 @@
+//tab click
 $('.tab').on('mousehover', function(){
   if($(this).data('content')){
     $('section').hide();
     $('.' + $(this).attr('data-content')).fadeIn();
   }
   $('.article').click();
+});
+// select box
+$('#category-filter').on('change', function(){
+  if($(this).val()){
+    $('.article').hide();
+    $('.article[data-category=' + $(this).val() + ']').fadeIn();
+  }
 });

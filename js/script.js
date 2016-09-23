c9ec8988c30b34be62a12ca30d8b06bfb36f7a12
Bhavya Bharti <bhavyab@outlook.com>
2016-09-23 10:28:48 -0700

    category is working

--- js/script.js
+++ js/script.js
@@ -3,8 +3,14 @@ var template = Handlebars.compile(source); //compile it
 var theCompiledHtml = template(myContext); //
 $('.home').append(theCompiledHtml);
 
-//filter creation
+var fObj = { filter: ($.unique(myContext.myLocaldata.map(function(index){  //generate category from each article and pass to $.unique jQuery function to generate unique article category.
+  return index.category;
+}))).map(function(item){  //converted arry into object as it handelbar template recieves object as parameter.
+  return {category: item};
+}),
+};
+// grab the template in html and populate with category and invoke to the DOM.
 var sourceC = $('#category-option-template').html();
 var templateC = Handlebars.compile(sourceC);
-var theCompiledHtmlC = templateC(myContext);
+var theCompiledHtmlC = templateC(fObj);
 $('#category-filter').append(theCompiledHtmlC);

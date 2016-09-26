46b131632ecbf891c3627e405d5d0b1df7defc17
Bhavya Bharti <bhavyab@outlook.com>
2016-09-26 09:54:25 -0700

    profile ready for review.

--- js/viewPage.js
+++ js/viewPage.js
@@ -2,17 +2,29 @@
 $('.tab').on('mouseover', function(){
   if($(this).data('content')){
     $('.tab').on('click', function(){
-      $('section').hide();
+      $('main > section').hide();
       $('.' + $(this).attr('data-content')).fadeIn();
     });
-
     $('.article').click();
+    //about sections project tab click
+    if($(this).attr('data-content') === 'about'){
+      $('.contact').hide();
+    }
   }});
 // select box
 $('#category-filter').on('change', function(){
   if($(this).val()){
-    console.log($(this).val());
     $('.article').hide();
     $('.article[data-category=' + $(this).val() + ']').fadeIn();
+  }
+  if(($(this).val()) === ''){$('.article').fadeIn();}
+});
+//show and hide project and contact section
+$('.myTab').on('click', function(){
+  if($(this).data('content')){
+    $('.myTab').on('click', function(){
+      $('.about > section').hide();
+      $('.'+ $(this).attr('data-content')).fadeIn();
+    });
   }
 });

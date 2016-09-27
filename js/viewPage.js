81f2f3d9d707fe047452f14256ba3d07a4bddd72
Bhavya Bharti <bhavyab@outlook.com>
2016-09-27 12:13:40 -0700

    project and contact is clickable

--- js/viewPage.js
+++
@@ -1,8 +0,0 @@
-// select box
-$('#category-filter').on('change', function(){
-  if($(this).val()){
-    $('.article').hide();
-    $('.article[data-category=' + $(this).val() + ']').fadeIn();
-  }
-  if(($(this).val()) === ''){$('.article').fadeIn();}
-});

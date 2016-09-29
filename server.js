21467ac8eca549b52488907f95d0f10f780dbafc
Bhavya Bharti <bhavyab@outlook.com>
2016-09-29 10:25:39 -0700

    server is modified to call proxy-express

--- server.js
+++ server.js
@@ -1,7 +1,20 @@
 // Let's build a server!
 var express = require('express'),
+
+//PROXY request
+  requestProxy = require('express-request-proxy'),
   port = process.env.PORT || 3000,
   app = express();
+
+//proxy
+var proxyGitHub = function(request, response) {
+  console.log('Routing GitHub request for', request.params[0]);
+  (requestProxy({
+    url: 'https://api.github.com/' + request.params[0],
+    headers: {Authorization: 'token ' + process.env.GITHUB_TOKEN}
+  }))(request, response);
+};
+app.get('/github/*', proxyGitHub);
 
 app.use(express.static('./'));
 

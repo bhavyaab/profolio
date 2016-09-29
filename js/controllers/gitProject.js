31e3442c008e3290e8cbd0a1539b504351db378a
Bhavya Bharti <bhavyab@outlook.com>
2016-09-29 09:10:53 -0700

    debug

--- js/controllers/gitProject.js
+++ js/controllers/gitProject.js
@@ -3,10 +3,9 @@ var project;
 $.ajax({
   url:'https://api.github.com/users/bhavyaab/repos',
   type: 'GET',
-  header: {'Authorization': 'token '+ 'd84b247301c2e6b81481aefd520afd60ff5d0c6f'},
+  header: {'Authorization': 'token '+ gitToken},
   success: function(data, message, xhr){
-    project = data.filter(function(item){return item.stargazers_count > 0;}).slice();
+    project = data.filter(function(item){return item.watchers_count > 0;}).slice();
     git();
   }
 });
-var x = project.filter(function(item){return item.watchers_count > 0;});
